f37de3163e8da35c7345c800e03d316e8b5c975a
Arief Aliman <4r1efaliman@gmail.com>
2022-12-09 10:43:12 +0700

    update kasir

--- resources/views/kasir/index_tambah.blade.php
+++ resources/views/kasir/index_tambah.blade.php
@@ -2,6 +2,24 @@
 
 @section('containers')
 
+<script type='text/javascript'>
+  $(document).ready(function() {
+    var wrapper = $("#container");
+    var add_button = $("#btn");
+
+    $(add_button).click(function(e) {
+        e.preventDefault();
+        $(wrapper).append('<div class="form-row"><div class="card shadow col-md-8" id="card"><div class="card-body"><div class="row justify-content-center"><select class="form-control col-md-8" name="kategori_id[]" id="kategori"><option selected>Pilih Kategori</option>       @foreach ($categories as $category)<option value="{{ $category->id }}">{{ $category->nama_kategori }}</option>@endforeach</select><input class="form-control col-md-3 ml-3" type="text" name="berat[]" placeholder="/ KG"></div></div></div><button type="button" class="btn btn-primary col-md-2 ml-3" style="height: 45px; margin: auto" id="delete">+</button></div>'); //add input box
+    });
+
+    $(wrapper).on("click", "#delete", function(e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    })
+});
+</script>
+
 <div class="container-fluid pt-4 mb-5 ">
     <form action="/kasir/laundry" method="post">
       @csrf
@@ -39,24 +57,35 @@
             </select>
           </div>
         </div>
-        <div class="form-row justify-content-start">
+        <div class="form-row justify-content-center">
           <div class="form-group">
-            <div class="form-check">
-              <div class="card shadow">
+            <div class="form-row">
+              <div class="card shadow col-md-8" id="card">
                 <div class="card-body">
-                  <select class="form-control" name="kategori_id[]" id="kategori">
-                    <option selected>Pilih Kategori</option>
-                    @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->nama_kategori }}</option>
-                    @endforeach
-                  </select>
+                  <div class="row justify-content-center">
+                    <select class="form-control col-md-8" name="kategori_id[]" id="kategori">
+                      <option selected>Pilih Kategori</option>
+                      @foreach ($categories as $category)
+                      <option value="{{ $category->id }}">{{ $category->nama_kategori }}</option>
+                      @endforeach
+                    </select>
+                    <input class="form-control col-md-3 ml-3" type="text" name="berat[]" placeholder="/ KG">
+                  </div>
                 </div>
               </div>
+              <button type="button" class="btn btn-primary col-md-2 ml-3" style="height: 45px; margin: auto" id="btn">+</button>
             </div>
-            <div class="mt-3">
-              <button type="submit" class="btn btn-primary">Tambah Cucian</button>
-              <a href="{{ url('/kasir/laundry') }}" class="btn btn-primary ml-3" style="width: 115px">Batal</a>
-            </div>
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="form-group" id="container">
+            
+          </div>
+        </div>
+        <div class="form-row justify-content-center">
+          <div class="mt-3">
+            <button type="submit" class="btn btn-primary">Tambah Cucian</button>
+            <a href="{{ url('/kasir/laundry') }}" class="btn btn-primary ml-3" style="width: 115px">Batal</a>
           </div>
         </div>
     </form>
